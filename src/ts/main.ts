61b86ce77db9be010d1c927e3be8af61882d5085
Matei Adriel <rafaeladriel11@gmail.com>
2019-12-22 22:36:59 +0200

    feat: started over

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,130 +1,187 @@
 import { Engine, Render, Runner, Events, World, Bodies, Body } from "matter-js";
 import { planet, house } from "./assets";
 
-export const main = () => {
-  const width = window.innerWidth;
-  const height = window.innerHeight;
+enum CellType {
+  frozen,
+  full,
+  empty
+}
+
+type Cell = CellType;
+
+type Layout = {
+  width: number;
+  height: number;
+  cells: Cell[];
+};
+
+const loadImage = (w: number, h: number, url: string) => {
+  const img = new Image(w, h);
+
+  img.src = url;
+
+  return [
+    img,
+    new Promise(resolve => {
+      img.onload = resolve;
+    })
+  ] as const;
+};
+
+const [snow, snowLoaded] = loadImage(
+  256,
+  256,
+  require("../../assets/snow.png")
+);
+
+const [present, presentLoaded] = loadImage(
+  16,
+  16,
+  require("../../assets/present.png")
+);
+
+const thingsToLoad = [presentLoaded, snowLoaded];
+
+const drawTile = (
+  _type: CellType,
+  context: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  width: number,
+  height: number
+) => {
+  // console.log({ _type });
+
+  if (_type === CellType.frozen) {
+    context.drawImage(snow, 32, 0, 32, 32, x, y, width, height);
+  } else if (_type === CellType.empty) {
+    context.fillStyle = "black";
+    context.fillRect(x, y, width, height);
+  } else {
+    context.drawImage(present, x, y, width, height);
+  }
+};
+
+const getTilePosition = (index: number, width: number) => {
+  const x = index % width;
+  const y = Math.floor(index / width);
+  return [x, y];
+};
+
+export const main = async () => {
+  let width: number, height: number;
 
   const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  const context = canvas.getContext("2d");
 
-  // create engine
-  const engine = Engine.create();
-  const { world } = engine;
+  const resize = () => {
+    width = window.innerWidth;
+    height = window.innerHeight;
 
-  const zoom = 1;
-
-  // create renderer
-  const renderer = Render.create({
-    element: document.body,
-    engine,
-    options: {
-      width,
-      height,
-      wireframes: false,
-      background: "black"
-    },
-    bounds: {
-      min: { x: 0, y: 0 },
-      max: { x: width * zoom, y: height * zoom }
-    },
-    canvas
-  });
-
-  Render.run(renderer);
-
-  // create runner
-  const runner = Runner.create();
-  Runner.run(runner, engine);
-
-  // remove gravity
-  engine.world.gravity.y = 0;
-
-  const planetRadius = 1000;
-  const planetSpeed = 0.003;
-  // planet
-  const planetBody = Bodies.circle(
-    width / 2,
-    height / 2 + planetRadius,
-    planetRadius,
-    {
-      render: {
-        sprite: planet(planetRadius * 2 + 495)
-      },
-      mass: Infinity,
-      label: "planet",
-      restitution: 0
-    }
-  );
-
-  World.add(world, planetBody);
-
-  const spawnHouse = () => {
-    const size = 2;
-
-    const _width = 100;
-    const _height = 150;
-
-    const houseBody = Bodies.rectangle(width / 2, 400, _width, _height, {
-      render: {
-        sprite: house(_width, _height)
-      },
-      mass: 1000,
-      label: "house",
-      frictionAir: 0
-    });
-
-    World.add(world, houseBody);
+    canvas.width = width;
+    canvas.height = height;
   };
 
-  const spawnPizza = () => {
-    const ship = Bodies.rectangle(width / 2, 200, 70, 10, {
-      render: {
-        fillStyle: "white"
-      },
-      frictionAir: 0
-    });
+  await Promise.all(thingsToLoad);
 
-    ship.force.x += 0.002;
+  const render = (layout: Layout) => {
+    context.fillStyle = "#333333";
+    context.fillRect(0, 0, width, height);
 
-    // test box
-    World.add(world, ship);
-  };
+    const minD = Math.min(width, height);
+    const side = minD / (Math.min(layout.height, layout.width) + 2);
 
-  const houseSpacing = 0.7;
-  let lastHouse = houseSpacing;
-
-  Events.on(engine, "afterUpdate", () => {
-    for (const body of world.bodies) {
-      if (body !== planetBody) {
-        const Dx = planetBody.position.x - body.position.x;
-        const Dy = planetBody.position.y - body.position.y;
-        const force = body.label === "house" ? 1 : 0.0002;
-        const angle = Math.atan2(Dy, Dx);
-        body.force.x += force * Math.cos(angle);
-        body.force.y += force * Math.sin(angle);
-      }
-
-      if (
-        body.label === "house" &&
-        body.position.y > height / 2 + planetRadius * 2 - 50
-      ) {
-        World.remove(world, body);
+    for (let x = 0; x < width / side; x++) {
+      for (let y = 0; y < height / side; y++) {
+        drawTile(CellType.frozen, context, x * side, y * side, side, side);
       }
     }
 
-    Body.setAngularVelocity(planetBody, planetSpeed);
-    Body.setPosition(planetBody, {
-      x: width / 2,
-      y: height / 2 + planetRadius
-    });
+    const tilesFitting = [width / side, height / side].map(Math.floor);
 
-    lastHouse++;
+    context.save();
+    context.translate(
+      Math.round((tilesFitting[0] - layout.width) / 2) * side,
+      Math.round((tilesFitting[1] - layout.height) / 2) * side
+    );
 
-    if (lastHouse >= houseSpacing / planetSpeed) {
-      lastHouse = 0;
-      spawnHouse();
+    for (let index = 0; index < layout.cells.length; index++) {
+      const cell = layout.cells[index];
+
+      const [x, y] = getTilePosition(index, layout.width);
+
+      drawTile(cell, context, x * side, y * side, side, side);
     }
+
+    context.restore();
+  };
+
+  let base = {
+    width: 3,
+    height: 3,
+    cells: [
+      CellType.frozen,
+      CellType.empty,
+      CellType.frozen,
+      CellType.empty,
+      CellType.empty,
+      CellType.empty,
+      CellType.frozen,
+      CellType.empty,
+      CellType.frozen
+    ]
+  };
+
+  const loop = () => {
+    render(base);
+
+    requestAnimationFrame(loop);
+  };
+
+  resize();
+  loop();
+
+  window.onresize = resize;
+
+  const doTurn = (e: MouseEvent) => {
+    const layout = base;
+    const minD = Math.min(width, height);
+    const side = minD / (Math.min(layout.height, layout.width) + 2);
+
+    const tilesFitting = [width / side, height / side].map(Math.floor);
+
+    const [x, y] = [
+      e.clientX - Math.round((tilesFitting[0] - layout.width) / 2) * side,
+      e.clientY - Math.round((tilesFitting[1] - layout.height) / 2) * side
+    ];
+
+    const [cx, cy] = [x / side, y / side].map(Math.floor);
+    const mouseIndex = cy * layout.width + cx;
+    const newLayout = {
+      ...layout,
+      cells: layout.cells.map((v, i) =>
+        mouseIndex === i && v === CellType.empty ? CellType.full : v
+      )
+    };
+
+    base = newLayout;
+  };
+
+  let mouseDown = false;
+
+  canvas.addEventListener("mouseup", () => {
+    mouseDown = false;
   });
 
-  canvas.addEventListener("click", spawnPizza);
+  canvas.addEventListener("mousedown", e => {
+    mouseDown = true;
+
+    doTurn(e);
+  });
+
+  canvas.addEventListener("mousemove", e => {
+    if (mouseDown) {
+      doTurn(e);
+    }
+  });
 };
